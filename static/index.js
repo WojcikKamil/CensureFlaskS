00a2a108117a847d5d165f0398cb1a2367af20ba
Kamil Wójcik <60044272+WojcikKamil@users.noreply.github.com>
2021-06-28 10:10:28 +0200

    System for uploading and displaying photos

---
+++ static/index.js
@@ -0,0 +1,49 @@
+window.onload = () => {
+	$('#sendbutton').click(() => {
+		imagebox = $('#imagebox')
+		input = $('#imageinput')[0]
+		if(input.files && input.files[0])
+		{
+			let formData = new FormData();
+			formData.append('image' , input.files[0]);
+			$.ajax({
+				url: "http://localhost:5000/Censure",
+				type:"POST",
+				data: formData,
+				cache: false,
+				processData:false,
+				contentType:false,
+				error: function(data){
+					console.log("upload error" , data);
+					console.log(data.getAllResponseHeaders());
+				},
+				success: function(data){
+					console.log(data);
+					bytestring = data['status']
+					image = bytestring.split('\'')[1]
+					imagebox.attr('src' , 'data:image/jpeg;base64,'+image)
+				}
+			});
+		}
+	});
+};
+
+
+
+function readUrl(input){
+	imagebox = $('#imagebox')
+	console.log("evoked readUrl")
+	if(input.files && input.files[0]){
+		let reader = new FileReader();
+		reader.onload = function(e){
+			// console.log(e)
+
+			imagebox.attr('src',e.target.result);
+			imagebox.maxheight(1000);
+			imagebox.maxwidth(800);
+		}
+		reader.readAsDataURL(input.files[0]);
+	}
+
+
+}
