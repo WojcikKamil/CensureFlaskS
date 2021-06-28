6c50f2eed4df85a15fe90e41234dfa79b6639127
Kamil Wójcik <60044272+WojcikKamil@users.noreply.github.com>
2021-06-28 10:21:23 +0200

    Added LM

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
-from flask import Flask, render_template
+import base64
+import io
+import sys
+import cv2
+import cv2 as cv
+import numpy as np
+from PIL import Image
+from flask import Flask, render_template, request, jsonify
+from disp import Disp
 
 app = Flask(__name__)
 
-app.route('/')
+
+@app.route('/Censure', methods=['POST'])
+def censure_image():
+    censure = cv.CascadeClassifier('static/cascade/cascade.xml')
+    disp_censure = Disp(None)
+    # print(request.files , file=sys.stderr)
+    file = request.files['image'].read()
+    npimg = np.frombuffer(file, np.uint8)
+    img = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
+    # fixing color bug
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #img = cv.imshow(npimg)
+    rectangles = censure.detectMultiScale(img)
+    detection_image = disp_censure.draw_rectangles(img, rectangles)
+    cv.imshow('Matches', detection_image)
+
+    img = Image.fromarray(img.astype("uint8"))
+    rawBytes = io.BytesIO()
+    img.save(rawBytes, "JPEG")
+    rawBytes.seek(0)
+    img_base64 = base64.b64encode(rawBytes.read())
+    return jsonify({'status': str(img_base64)})
+
+
+@app.route('/')
 def home():
     return render_template('./index.html')
+
+
+@app.after_request
+def after_request(response):
+    print("log: setting cors", file=sys.stderr)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 
 if __name__ == '__main__':
---
+++ disp.py
@@ -0,0 +1,75 @@
+import cv2 as cv
+import numpy as np
+
+
+class Disp:
+    # constants
+    TRACKBAR_WINDOW = "Trackbars"
+
+    # properties
+    needle_img = None
+    needle_w = 0
+    needle_h = 0
+    method = None
+
+    # constructor
+    def __init__(self, needle_img_path, method=cv.TM_CCOEFF_NORMED):
+        if needle_img_path:
+            # load the image we're trying to match
+            # https://docs.opencv.org/4.2.0/d4/da8/group__imgcodecs.html
+            self.needle_img = cv.imread(needle_img_path, cv.IMREAD_UNCHANGED)
+
+            # Save the dimensions of the needle image
+            self.needle_w = self.needle_img.shape[1]
+            self.needle_h = self.needle_img.shape[0]
+
+        # There are 6 methods to choose from:
+        # TM_CCOEFF, TM_CCOEFF_NORMED, TM_CCORR, TM_CCORR_NORMED, TM_SQDIFF, TM_SQDIFF_NORMED
+        self.method = method
+
+    def find(self, haystack_img, threshold=0.5, max_results=10):
+        # run the OpenCV algorithm
+        result = cv.matchTemplate(haystack_img, self.needle_img, self.method)
+
+        # Get the all the positions from the match result that exceed our threshold
+        locations = np.where(result >= threshold)
+        locations = list(zip(*locations[::-1]))
+        # print(locations)
+
+        # if we found no results, return now. this reshape of the empty array allows us to
+        # concatenate together results without causing an error
+        if not locations:
+            return np.array([], dtype=np.int32).reshape(0, 4)
+
+        # You'll notice a lot of overlapping rectangles get drawn. We can eliminate those redundant
+        # locations by using groupRectangles().
+        # First we need to create the list of [x, y, w, h] rectangles
+        rectangles = []
+        for loc in locations:
+            rect = [int(loc[0]), int(loc[1]), self.needle_w, self.needle_h]
+            # Add every box to the list twice in order to retain single (non-overlapping) boxes
+            rectangles.append(rect)
+            rectangles.append(rect)
+       
+        rectangles, weights = cv.groupRectangles(rectangles, groupThreshold=1, eps=0.5)
+        # print(rectangles)
+
+        if len(rectangles) > max_results:
+            print('Warning: too many results, raise the threshold.')
+            rectangles = rectangles[:max_results]
+
+        return rectangles
+
+    def draw_rectangles(self, haystack_img, rectangles):
+        # these colors are actually BGR
+        line_color = (0, 255, 0)
+        line_type = cv.LINE_4
+
+        for (x, y, w, h) in rectangles:
+            # determine the box positions
+            top_left = (x, y)
+            bottom_right = (x + w, y + h)
+            # draw the box
+            cv.rectangle(haystack_img, top_left, bottom_right, line_color, lineType=line_type)
+
+        return haystack_img
